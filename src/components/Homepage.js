e96c4ccce7cc26cbec880d2e7ff232517fc3de6b
Darsh04-14 <darshpatelcr7@gmail.com>
2023-01-27 17:02:33 -0500

    Made navbar responsive and made small changes

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import {React, useState} from 'react';
 import { Link } from "react-router-dom";
 import pImage from './predAnalysis.svg';
 import './styles.css';
 import Features from './Features';
 
 function HomePage() {
+    const [scrollActive, setScrollActive] = useState('false');
+    function isScroll() {
+        if (window.scrollY > 0) setScrollActive(true);
+        else setScrollActive(false);
+    }
+    window.addEventListener('scroll', isScroll);
     return (
     <div className="homepage-container">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-        <div className="navbar-container">
+        <div className={scrollActive ? 'navbar-container navbar-scroll' : 'navbar-container'}>
             <h1 className="logo">{'</INV'}<span className="green">$</span>{'T/>'}</h1>
             <div className="nav-options-container">
                 <span>Services</span>
