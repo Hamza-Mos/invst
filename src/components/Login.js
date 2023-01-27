ee08992ccd49963855e5eba826fb6e9b2e13d3b2
sourishdas07 <109391867+sourishdas07@users.noreply.github.com>
2023-01-26 21:52:25 -0500

    login screen init

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,23 +2,54 @@ import React from 'react';
 import SignUpScreen from './SignUpScreen';
 import './Login.css';
 
+// login-container
+
 function LogIn() {
     return (
       <div
-        className="login-container"
+        className="loginScreen"
         style={{
           backgroundSize: "cover",
-          backgroundImage: `url("https://thumbs.dreamstime.com/b/stock-market-graph-big-data-visualization-investment-graph-concept-stock-market-graph-big-data-visualization-investment-graph-145653496.jpg")`,
+          backgroundImage: `url("http://s3-us-west-2.amazonaws.com/techvibes/wp-content/uploads/2017/04/24135159/Netflix-Background.jpg")`,
         }}
       >
         <div className="loginScreen__background">
           <img
             className="loginScreen_logo"
-            src="https://thumbs.dreamstime.com/b/stock-market-graph-big-data-visualization-investment-graph-concept-stock-market-graph-big-data-visualization-investment-graph-145653496.jpg"
-            alt="Hello"
+            src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+            alt=""
           />
 
+          <button
+            className="loginScreen__button"
+          >
+            Sign In
+          </button>
+
           <div className="loginScreen__gradient" />
+        </div>
+
+        <div className="loginScreen_body">
+
+              <h1>Unlimited films, TV programmes and more.</h1>
+              <h2>Watch anywhere. Cancel at any time.</h2>
+              <h3>
+                Ready to watch? Enter your email to create or restart your
+                membership.
+              </h3>
+
+              <div className="loginScreen_input">
+                <form>
+                  <input type="email" placeholder="Email Address" />
+                </form>
+                <button
+                  className="loginScreen_getStarted"
+                >
+                  {" "}
+                  GET STARTED
+                </button>
+              </div>
+
         </div>
       </div>
     );
