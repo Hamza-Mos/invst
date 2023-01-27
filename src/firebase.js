8fd3fea8fae0cc44ba8198022412fbe7acc46533
Darsh04-14 <darshpatelcr7@gmail.com>
2023-01-26 21:02:43 -0500

    Firebase Stuff

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/combat/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDRh-VF6t4a-x5jk8shSPQ_H3sMyEUtxDo",
+  authDomain: "invest-b61ac.firebaseapp.com",
+  projectId: "invest-b61ac",
+  storageBucket: "invest-b61ac.appspot.com",
+  messagingSenderId: "423293677519",
+  appId: "1:423293677519:web:9e11655efa1da6bdbcacba",
+  measurementId: "G-NCLF2YLYJ2"
+};
+
