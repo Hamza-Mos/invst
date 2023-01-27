5e2715a901bc2376d8723da2c5f3abdb1e57c0dd
sourishdas07 <109391867+sourishdas07@users.noreply.github.com>
2023-01-27 06:04:30 -0500

    init features

--- src/components/Features.js
+++ src/components/Features.js
@@ -3,6 +3,11 @@ import React from 'react'
 function Features() {
   return (
     <div>
+      <div className="button-alert-bot">
+        <h1>Stock Price Alarm Bot</h1>
+        <button>Try It Out</button>
+      </div>
+
       <h1>TEST</h1>
       <h1>TEST</h1>
       <h1>TEST</h1>
@@ -17,7 +22,6 @@ function Features() {
       <h1>TEST</h1>
       <h1>TEST</h1>
       <h1>TEST</h1>
-      
     </div>
   );
 }
