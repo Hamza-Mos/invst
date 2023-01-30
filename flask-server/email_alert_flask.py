b62005cc81424375a3ed23f448a8a841508e1357
sourishdas07 <109391867+sourishdas07@users.noreply.github.com>
2023-01-29 20:37:44 -0500

    working on flask

--- flask-server/email_alert_flask.py
+++ flask-server/email_alert_flask.py
@@ -2,11 +2,12 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 # Members API Route
-@app.route("/members")
+@app.route("/members", methods=["GET"])
 def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+    return {}
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
--- src/Backend/email_alert.py
+++ src/Backend/email_alert.py
@@ -3,16 +3,22 @@ import matplotlib.pyplot as plt
 import time
 import yfinance as yf
 
-from flask import Flask
-
 # Automating Emails
+from flask import request
 import smtplib
 import ssl
 from email.message import EmailMessage
 
-ticker = input('Ticker Symbol: ')
+# Flask Imports
+from flask import Flask
+from flask_cors import CORS, cross_origin
 
-target_sell_price = int(input('Target Sell Price: '))
+# Init Flask
+app = Flask(__name__)
+CORS(app)
+
+# ticker = input('Ticker Symbol: ')
+# target_sell_price = int(input('Target Sell Price: '))
 
 
 def get_current_price(symbol):
@@ -20,26 +26,39 @@ def get_current_price(symbol):
     todays_data = ticker.history(period='1min')
     return todays_data['Close'][0]
 
-if get_current_price(ticker) > target_sell_price:
+def emailing(ticker, target_sell_price):
+    if get_current_price(ticker) > target_sell_price:
 
-    email_sender = 'sourish10das@gmail.com'
-    password = 'bvzpghjfblusqodp'
-    email_receiver = input('Recipient Email: ') # We have to receive this from firebase
+        email_sender = 'sourish10das@gmail.com'
+        password = 'bvzpghjfblusqodp'
+        email_receiver = input('Recipient Email: ') # We have to receive this from firebase
 
-    # Set the subject and body of the email
-    subject = input('ALERT')
-    body = input("""Body: """)
+        # Set the subject and body of the email
+        subject = input('ALERT')
+        body = input("""Body: """)
 
-    em = EmailMessage()
-    em['From'] = email_sender
-    em['To'] = email_receiver
-    em['Subject'] = subject
-    em.set_content(body)
+        em = EmailMessage()
+        em['From'] = email_sender
+        em['To'] = email_receiver
+        em['Subject'] = subject
+        em.set_content(body)
 
-    # Add SSL (layer of security)
-    context = ssl.create_default_context()
+        # Add SSL (layer of security)
+        context = ssl.create_default_context()
 
-    # Log in and send the email
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-        smtp.login(email_sender, password)
-        smtp.sendmail(email_sender, email_receiver, em.as_string())
+        # Log in and send the email
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+            smtp.login(email_sender, password)
+            smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+
+@app.route("/members", methods=["POST"])
+def members():
+    ticker = request.json['ticker']
+    target_sell_price = request.json['target_sell_price']
+    email = request.json['email']
+
+    return ticker
+
+if __name__ == "__main__":
+    app.run(debug=True)
